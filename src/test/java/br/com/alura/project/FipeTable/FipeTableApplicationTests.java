7bf79c7c5a93b5d12a02c183c2000b8b2991890f
Maria Ferreira <maria.ferreira312017@gmail.com>
2025-06-11 13:57:42 +0800

    first commit

---
+++ src/test/java/br/com/alura/project/FipeTable/FipeTableApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.project.FipeTable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
